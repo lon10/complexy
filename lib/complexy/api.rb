ece71aaba7a1d2dad293e27fd560708118962579
Rustam Manakov <rustam@MacBook-Air-Rustam.local>
2017-08-25 17:07:41 +0300

    Simple routes

--- lib/complexy/api.rb
+++ lib/complexy/api.rb
@@ -6,5 +6,19 @@ module Complexy
     get do
       { message: 'Hello, this is complexy service' }
     end
+
+    namespace :users do
+      desc 'Read users'
+      get do
+        Storage::User.all
+      end
+    end
+
+    namespace :activities do
+      desc 'Read activities'
+      get do
+      	Storage::UserActivity.all
+      end
+    end
   end
 end
